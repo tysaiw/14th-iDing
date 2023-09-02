3d5ec8f46c02ff02247f19b9ee6895333ed86ad8
Aiko <tysaiw@gmail.com>
2023-09-02 12:00:57 +0800

    add debug and logger

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ Rails.application.configure do
 
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).
-  config.log_level = :info
+  config.log_level = debug
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
@@ -92,4 +92,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.hosts << "iding.cc"
+  
+  config.logger = ActiveSupport::Logger.new(STDOUT)
 end
